a7bfa924b648bb0ad9b69ef775b8e335ed7661b4
Connor Van Etten <87097190+Crvanetten15@users.noreply.github.com>
2024-06-24 22:26:45 -0400

    feat : update content

--- src/components/Experiments.jsx
+++ src/components/Experiments.jsx
@@ -5,19 +5,19 @@ const items = [
   {
     href: '',
     iconClass: 'fa-solid fa-brush',
-    title: 'Title 1',
+    title: 'Squat Physics Simulation',
     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit sit eius eligendi alias ipsam, nulla, quas tenetur incidunt pariatur magni perspiciatis possimus, temporibus necessitatibus sed. Amet accusantium ab unde aut.',
   },
   {
     href: '',
     iconClass: 'fa-solid fa-brush',
-    title: 'Title 2',
+    title: 'Conway\'s Game of Life',
     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit sit eius eligendi alias ipsam, nulla, quas tenetur incidunt pariatur magni perspiciatis possimus, temporibus necessitatibus sed. Amet accusantium ab unde aut.',
   },
   {
     href: '',
     iconClass: 'fa-solid fa-brush',
-    title: 'Title 3',
+    title: '8-bit Animation of Levi',
     description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit sit eius eligendi alias ipsam, nulla, quas tenetur incidunt pariatur magni perspiciatis possimus, temporibus necessitatibus sed. Amet accusantium ab unde aut.',
   },
   // Add more items as needed
